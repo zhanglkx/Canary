b56fbcfff639808e2fb7411eba25e6866edbd7c7
张理科 <zhanglkx@foxmail.com>
2025-12-20 21:32:40 +0800

    init

--- apps/api/src/category/dto/update-category.input.ts
+++
@@ -1,5 +0,0 @@
-import { InputType, PartialType } from '@nestjs/graphql';
-import { CreateCategoryInput } from './create-category.input';
-
-@InputType()
-export class UpdateCategoryInput extends PartialType(CreateCategoryInput) {}
